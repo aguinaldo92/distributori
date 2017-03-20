577c4e1aa6b38ac396abf11c52133489d907d3be
Salvatore <sato89@hotmail.it>
2017-03-20 17:24:27 +0100

    Realizzate le Suite di Tests per i package con più di un test.

---
+++ test/it/unisalento/distributori/daoimpl/AllDaoImplTests.java
@@ -0,0 +1,14 @@
+package it.unisalento.distributori.daoimpl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CategoriaDaoImplTest.class, CategorieForniteDaoImplTest.class, DipendenteDaoImplTest.class,
+		DistributoreDaoImplTest.class, FamigliaDaoImplTest.class, FamiglieProdottoDaoImplTest.class,
+		FeedbackDaoImplTest.class, PersonaDaoImplTest.class, ProdottiErogatiDaoImplTest.class,
+		ProdottoDaoImplTest.class, ProduttoreDaoImplTest.class, StabilimentoDaoImplTest.class })
+public class AllDaoImplTests {
+
+}
---
+++ test/it/unisalento/distributori/util/AllUtilTests.java
@@ -0,0 +1,11 @@
+package it.unisalento.distributori.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GeneraPwdTest.class, PermissionsHashMapTest.class, SendMailSSLTest.class })
+public class AllUtilTests {
+
+}
