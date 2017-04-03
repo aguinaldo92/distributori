6fbc867d48448450e4f8439e8700085cf4d10278
Salvatore <sato89@hotmail.it>
2017-04-03 22:52:29 +0200

    Modifica di piccole imprecisioni

--- src/it/unisalento/distributori/dao/PersonaDao.java
+++ src/it/unisalento/distributori/dao/PersonaDao.java
@@ -1,9 +1,6 @@
 package it.unisalento.distributori.dao;
 
-import java.util.List;
-
 import it.unisalento.distributori.domain.Persona;
-import it.unisalento.distributori.model.PersonaModel;
 
 public interface PersonaDao extends BaseDao<Persona>{
 	
