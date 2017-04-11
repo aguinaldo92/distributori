2e6fc648e756c9fc4ffb55dddbb2ee3da928357b
Salvatore <sato89@hotmail.it>
2017-04-11 12:04:47 +0200

    Aggiornate le Suite di test per package

--- test/it/unisalento/distributori/daoimpl/AllDaoImplTests.java
+++ test/it/unisalento/distributori/daoimpl/AllDaoImplTests.java
@@ -7,8 +7,9 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ CategoriaDaoImplTest.class, CategorieForniteDaoImplTest.class, DipendenteDaoImplTest.class,
 		DistributoreDaoImplTest.class, FamigliaDaoImplTest.class, FamiglieProdottoDaoImplTest.class,
-		FeedbackDaoImplTest.class, HibernateUtilTest.class, PersonaDaoImplTest.class, ProdottiErogatiDaoImplTest.class,
-		ProdottoDaoImplTest.class, ProduttoreDaoImplTest.class, StabilimentoDaoImplTest.class })
+		FeedbackDaoImplTest.class, HibernateUtilTest.class, ManutieneDaoImplTest.class, PersonaDaoImplTest.class,
+		ProdottiErogatiDaoImplTest.class, ProdottoDaoImplTest.class, ProduttoreDaoImplTest.class,
+		RifornisceDaoImplTest.class, StabilimentoDaoImplTest.class })
 public class AllDaoImplTests {
 
 }
--- test/it/unisalento/distributori/util/AllUtilTests.java
+++ test/it/unisalento/distributori/util/AllUtilTests.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GeneraPwdTest.class, PermissionsHashMapTest.class, SendMailSSLTest.class, ImageModifierTest.class })
+@SuiteClasses({ AddressTranslationTest.class, GeneraPwdTest.class, ImageModifierTest.class, PasswordUtilsTest.class,
+		PermissionsHashMapTest.class, SendMailSSLTest.class })
 public class AllUtilTests {
 
 }
